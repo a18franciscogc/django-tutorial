3354fb1673ba7568de458bfbc140a5010717e21f
Fco. Javier González Campos <a18franciscogc@iessanclemente.net>
2021-02-07 15:57:34 +0100

    urls.py

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -3,9 +3,6 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.urls import re_path
-from django.views.static import serve
-
 urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
